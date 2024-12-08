314abf848a2f31ec8b92349183aff6cd41d8ce0b
Aphisara Sonthi <nam.apsr.js@gmail.com>
2024-12-08 14:28:04 +0700

    up date

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,10 +1,107 @@
-import logo from './logo.png'
-import search_icon from './search_icon.png'
-import basket_icon from './basket_icon.png'
+import logo from './logo.jpg'
+import search_icon from './search_icon.jpg'
+import basket_icon from './basket_icon.jpg'
+import rating_starts from './rating_starts.png'
+import add_icon_white from './add_icon_white.png'
+import add_icon_red from './add_icon_red.png'
+import add_icon_green from './add_icon_green.png'
+
+import menu_0 from './menu_0.jpg'
+import menu_1 from './menu_1.jpg'
+import menu_2 from './menu_2.jpg'
+import menu_3 from './menu_3.jpg'
+import menu_4 from './menu_4.jpg'
+import menu_5 from './menu_5.jpg'
+import menu_6 from './menu_6.jpg'
+import menu_7 from './menu_7.jpg'
+import menu_8 from './menu_8.jpg'
+
+import icecream_1 from './icecream_1.jpg'
+import icecream_2 from './icecream_2.jpg'
+import icecream_3 from './icecream_3.jpg'
+import icecream_4 from './icecream_4.jpg'
+import icecream_5 from './icecream_5.jpg'
 
 export const assets = {
     logo,
     search_icon,
     basket_icon,
-
+    rating_starts,
+    add_icon_white,
+    add_icon_red,
+    add_icon_green
 }
+export const menu_list = [
+    {
+        menu_name: "Ice cream",
+        menu_image: menu_0
+    },
+    {
+        menu_name: "Cake",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Cookie",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Cup cake",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Waffle",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Donut",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Macaron",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Craker",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Milkshake",
+        menu_image: menu_8
+    }
+]
+
+export const food_list = [
+    {
+        _id: "5",
+        name: "Ice cream clone",
+        image: icecream_5,
+        price: 69,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Ice cream"
+    },
+    {
+        _id: "2",
+        name: "Ice cream stick",
+        image: icecream_2,
+        price: 45,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Ice cream"
+    },
+    {
+        _id: "3",
+        name: "Ice cream",
+        image: icecream_3,
+        price: 155,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Ice cream"
+    },
+    {
+        _id: "4",
+        name: "Ice cream",
+        image: icecream_4,
+        price: 179,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Ice cream"
+    }
+
+]
