56560a7ea2c6e98592489c7dd64c06e1e7b45d6b
Aphisara Sonthi <nam.apsr.js@gmail.com>
2024-12-09 01:27:35 +0700

    web frontend update

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from './logo.jpg'
+import logo from './logo.png'
 import search_icon from './search_icon.jpg'
 import basket_icon from './basket_icon.jpg'
 import rating_starts from './rating_starts.png'
@@ -21,6 +21,24 @@ import icecream_2 from './icecream_2.jpg'
 import icecream_3 from './icecream_3.jpg'
 import icecream_4 from './icecream_4.jpg'
 import icecream_5 from './icecream_5.jpg'
+
+import cake_1 from './cake_1.jpg'
+import cake_2 from './cake_2.jpg'
+import cake_3 from './cake_3.jpg'
+import cake_4 from './cake_4.jpg'
+import cake_5 from './cake_5.jpg'
+import cake_6 from './cake_6.jpg'
+
+import cookie_1 from './cookie_1.jpg'
+import cookie_2 from './cookie_2.jpg'
+import cookie_3 from './cookie_3.jpg'
+import cookie_4 from './cookie_4.jpg'
+import cookie_5 from './cookie_5.jpg'
+import cookie_6 from './cookie_6.jpg'
+import cookie_7 from './cookie_7.jpg'
+import cookie_8 from './cookie_8.jpg'
+import cookie_9 from './cookie_9.jpg'
+import cookie_10 from './cookie_10.jpg'
 
 export const assets = {
     logo,
@@ -72,7 +90,7 @@ export const menu_list = [
 
 export const food_list = [
     {
-        _id: "5",
+        _id: "1",
         name: "Ice cream clone",
         image: icecream_5,
         price: 69,
@@ -102,6 +120,134 @@ export const food_list = [
         price: 179,
         description: "test test test test test test test test test test test test test test test",
         category: "Ice cream"
+    },
+    {
+        _id: "5",
+        name: "Cake 1",
+        image: cake_1,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cake"
+    },
+    {
+        _id: "6",
+        name: "Cake 2",
+        image: cake_2,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cake"
+    },
+    {
+        _id: "7",
+        name: "Cake 3",
+        image: cake_3,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cake"
+    },
+    {
+        _id: "8",
+        name: "Cake 4",
+        image: cake_4,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cake"
+    },
+    {
+        _id: "9",
+        name: "Cake 5",
+        image: cake_5,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cake"
+    },
+    {
+        _id: "10",
+        name: "Cake 6",
+        image: cake_6,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cake"
+    },
+    {
+        _id: "11",
+        name: "Cookie 1",
+        image: cookie_1,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cookie"
+    },
+    {
+        _id: "12",
+        name: "Cookie 2",
+        image: cookie_2,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cookie"
+    },
+    {
+        _id: "13",
+        name: "Cookie 3",
+        image: cookie_3,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cookie"
+    },
+    {
+        _id: "14",
+        name: "Cookie 4",
+        image: cookie_4,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cookie"
+    },
+    {
+        _id: "15",
+        name: "Cookie 5",
+        image: cookie_5,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cookie"
+    },
+    {
+        _id: "16",
+        name: "Cookie 6",
+        image: cookie_6,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cookie"
+    },
+    {
+        _id: "17",
+        name: "Cookie 7",
+        image: cookie_7,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cookie"
+    },
+    {
+        _id: "18",
+        name: "Cookie 8",
+        image: cookie_8,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cookie"
+    },
+    {
+        _id: "19",
+        name: "Cookie 9",
+        image: cookie_9,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cookie"
+    },
+    {
+        _id: "20",
+        name: "Cookie 10",
+        image: cookie_10,
+        price: 79,
+        description: "test test test test test test test test test test test test test test test",
+        category: "Cookie"
     }
 
 ]
